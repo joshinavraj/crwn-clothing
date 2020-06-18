84b4d8edcb38c25b9cb5ac7d7b694558c6b60eab
navraj <joshinavraj@gmail.com>
2020-06-18 14:15:33 -0400

    updating our app with a shop page, collection preview componenbt, collection item component and their scss files.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'; 
+import ShopPage from './pages/shop/shop.component.jsx'
+import { Route, Switch } from "react-router";
 
+const HatsPage=()=>(
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+)
 function App() {
   return (
     <div >
-     <HomePage/>
+      <Switch>
+     <Route exact path='/' component={HomePage}/>
+     <Route exact path='/shop' component={ShopPage}/>
+     </Switch>
     </div>
   );
 }
